8a566286c3ed6204d0eead381857fe42609e6eb0
Malak Majed <134874997+malak1022002@users.noreply.github.com>
2023-09-22 20:43:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,338 @@
+<!--- اعتذاراتي يوجد عندي مشكلة لذلك حليت المشاريع بامتداد php 
+الفجوال عندي لا يفتح بامتداد html-->
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&display=swap" rel="stylesheet">
+<style>
+    .first{
+        font-size: 18px;
+    }
+    .pree{
+        color: black;
+    }
+    .hrr{
+        height: 1px;
+        width: 100%;
+        background-color: black;
+        color: #000;
+    }
+    .imm{
+        width: 550px;
+        height: 550px;
+    }
+    .foot{
+        color: #7D7D7D;
+        background-color: #F5F5F5;
+
+    }
+    span{
+        color: #000;
+    }
+    table{
+        margin-left: 90px;
+    }
+    footer{
+        margin-top: 90px;
+    }
+    .para{
+        margin-top: 50px;
+        margin-right: 900px;
+    }
+    .icon{
+        position: absolute;
+        font-size: 20px;
+    }
+    .insicon {
+    top: 20px;
+    left: 10px;
+}
+  .facicon {
+    top: 50px;
+    left: 10px;
+}
+.button{
+    border-radius: 0;
+    margin-left: 200px;
+}
+.fan{
+    width: 50%;
+    margin-top: 250px;
+    background-color: #EEC7BF;
+}
+.sect{
+    margin-top: 50px;
+}
+.here{
+    margin-top:20px;
+    margin-left: 200px;
+    font-size: 40px;
+}
+</style>
+  </head>
+<body>                
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="log">
+                <a class="navbar-brand" href="index.php">
+                    <img src="image/logo.png"
+                     class="logo" alt=""></a>
+                </div>
+                
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ABOUT US</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">PROJECTS</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    PAGES
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">BLOG</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">CONTACT</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">BUY NOW</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fa-solid fa-bars"></i></a>
+                            </li>
+                            </ul>
+    </div>
+  </div>
+</nav>
+<section class="home" id="home">
+    <div class="content sect">
+        <h4 class="here">We Are Here To</h4>
+        <h4 class="here">Build Your Dreams</h4>
+        <a href="#" class="btn btn-light button">VIEW MORE
+        <i class="fa-solid fa-arrow-right-long iconf"></i>
+</a>
+<nav class=" navbar-expand-lg fan">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link">Fb</a>
+        </li>&nbsp;&nbsp;&nbsp;&nbsp;
+        <li class="nav-item">
+          <a class="nav-link">In</a>
+        </li>&nbsp;&nbsp;&nbsp;&nbsp;
+        <li class="nav-item">
+          <a class="nav-link">Pn</a>
+        </li>&nbsp;&nbsp;&nbsp;&nbsp;
+        <li class="nav-item">
+          <a class="nav-link">Tw</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+    <div class="image">
+    <img src="image/home.png" alt="">
+    </div>
+    
+</section>
+<section id="about" class="about about-section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 col-12">
+                <div class="our about-text">
+                    <p class="text-secondary">03/06</p>
+                    <h2>Our Services</h2>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12 ps-lg-5 mt-md-5">
+                <div class="textt">
+<div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="cir">
+    <div class="circle">
+  </div>
+  <p class="text">Architecture Design</p>
+    </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+  <div class="col-sm-6">
+  <div class="cir">
+    <div class="circle">
+  </div>
+  <p class="text ext">Exterior Design</p>
+  <i class="fa-solid fa-arrow-right-long"></i>
+    </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
+<br><br>
+<div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+  <div class="cir">
+    <div class="circle">
+  </div>
+  <p class="text">Interior Design</p>
+    </div>        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+  <div class="col-sm-6">
+  <div class="cir">
+    <div class="circle">
+  </div>
+  <p class="text">Construction</p>
+    </div>        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="team" class="team section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-header text-center pb-5">
+                <p class="text-secondary">04/06</p>
+                    <h1>Our Expert Team</h1>
+                </div>
+            </div>
+            <div class="row padteam">
+  <div class="col-sm-4 mb-3 mb-sm-0">
+      <div class="image-container">
+            <img src="image/team-1.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram "></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Jancy Mitchaline</p>
+        <p class="card-text text-secondary textp">Designer</p>
+  </div>
+  <div class="col-sm-4 mb-3 mb-sm-0">
+  <div class="image-container">
+            <img src="image/team-2.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram"></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Mathew Shedlon</p>
+        <p class="card-text text-secondary textp">Architechi</p>
+</div>
+<div class="col-sm-4 mb-3 mb-sm-0">
+  <div class="image-container">
+            <img src="image/team-3.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram"></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Charlee Hoper</p>
+        <p class="card-text text-secondary textp">Designer</p>
+</div>
+        </div>
+        <div class="row padteam">
+  <div class="col-sm-4 mb-3 mb-sm-0">
+      <div class="image-container">
+            <img src="image/team-4.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram"></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Mitchaline</p>
+        <p class="card-text text-secondary textp">Designer</p>
+  </div>
+  <div class="col-sm-4 mb-3 mb-sm-0">
+  <div class="image-container">
+            <img src="image/team-5.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram"></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Shedlon Punch</p>
+        <p class="card-text text-secondary textp">Designer</p>
+</div>
+<div class="col-sm-4 mb-3 mb-sm-0">
+  <div class="image-container">
+            <img src="image/team-6.jpg" alt="" class="img">
+        <i class="fa-brands icon insicon fa-instagram"></i>
+        <i class="fa-brands icon facicon fa-facebook"></i>
+        </div>
+        <p class="card-text textteam">Scarlett Johansson</p>
+        <p class="card-text text-secondary textp">Designer</p>
+</div>
+        </div>
+    </div>
+
+</section>
+<!----------------------------------------------->
+<section id="awards" class="awards section-padding">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+                <div class="section-header text-center pb-5 para">
+                <p class="text-secondary">05/06</p>
+                <h1>The Awards</h1>
+                </div>
+            </div>
+        <table class="table table-borderless">
+  <tbody>
+    <tr>
+        <th rowspan="2">2018</th>
+        <th rowspan="2"><p class="first">Building Center Of Gloucester</p>
+  <pre class="text-secondary">New York Design Weekdummy text of</pre>
+  <pre class="text-secondary">the printing and industry</pre></th>
+        <th rowspan="9"><img src="image/m11.jpg" class="imm"></th>
+    </tr>
+    <tr>
+    </tr>
+    <tr>
+        <th colspan="2"><hr class="hrr"></th></th>
+    </tr>
+    <tr>
+        <th rowspan="2">2019</th>
+        <th rowspan="2"><p class="first">CSS Design Awards</p>
+        <pre class="text-secondary">UI,UX Inovation awards</pre>
+        <pre class="text-secondary">Best Construction Awards</pre></th>
+    </tr>
+    <tr>
+    </tr>
+    <tr>
+        <th colspan="2"><hr class="hrr"></th>
+    </tr>
+    <tr>
+        <th rowspan="2">2021</th>
+        <th rowspan="2"><p class="first">Top Archite</p>
+  <pre class="text-secondary">New York Design Weekdummy text of</pre>
+  <pre class="text-secondary">the printing and industry</pre></th>
+    </tr>
+    <tr>
+    </tr>
+    <tr>
+        <th colspan="2"><hr class="hrr"></th>
+    </tr>
+  </tbody>
+
+</table>
+        </div>
+        </div>
+</section>
+<footer>
+<div class="foot p-3 text-center">
+        <p>Copyright © Architype | Designed <span>VictorFlow</span> 
+        Templates - Powered by <span>VictorFlow </span>Webflow</p>
+      </div>
+</footer>
+        <!--End Header Upper-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>  </body>
+</html>
